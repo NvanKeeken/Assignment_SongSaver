de86ab944facd9ab769dc0d8e2f82693bdcdd6e6
N van Keeken <n.v.k20@gmail.com>
2022-12-07 21:04:23 +0100

    added navBar with React Router

---
+++ src/components/AboutPage.js
@@ -0,0 +1,14 @@
+import React from "react"
+import NavBar from "./NavBar"
+
+function AboutPage (){
+    return (
+        <div>
+        <NavBar/>
+        <h2>About</h2>
+        <p>This will be the about page....</p>
+        </div>
+    )
+}
+
+export default AboutPage
